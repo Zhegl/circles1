6434731ee8bd95dbd67e615ab9265b2acbe795ac
zhegl <zhegl123@gmail.com>
2020-11-10 21:22:48 +0300

    Добавлен случайный цвет

--- first.py
+++ first.py
@@ -4,12 +4,13 @@ from PyQt5 import uic  # Импортируем uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor
 from random import randint
+from ui_file import Ui_MainWindow
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('1.ui', self)  # Загружаем дизайн
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.run)
         self.test = None
         # Обратите внимание: имя элемента такое же как в QTDesigner
@@ -27,7 +28,7 @@ class MyWidget(QMainWindow):
             qp.end
 
     def draw_circle(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         self.radius = randint(0, self.height())
         self.x, self.y = randint(0, self.width()), randint(0, self.height())
         qp.drawEllipse(self.x - self.radius, self.y - self.radius, 2 * self.radius, 2 * self.radius)
